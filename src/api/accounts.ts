a08ade93fc732cd0bac8c10ff185a8160505fc3a
tony <tony.li@vechain.com>
2019-11-27 10:42:19 +0800

    update explore-db: use VTHO instead of Energy

--- src/api/accounts.ts
+++ src/api/accounts.ts
@@ -61,7 +61,7 @@ router.get('/:address/transactions', try$(async (req, res) => {
 }))
 
 // query type limit offset
-const AssetLiterals =  [  'VET', 'Energy', 'PLA', 'SHA', 'EHrT', 'DBET', 'TIC', 'OCE', 'SNK', 'JUR', 'AQD', 'YEET']
+const AssetLiterals =  [  'VET', 'VTHO', 'PLA', 'SHA', 'EHrT', 'DBET', 'TIC', 'OCE', 'SNK', 'JUR', 'AQD', 'YEET']
 router.get('/:address/transfers', try$(async (req, res) => {
     if (!isHexBytes(req.params.address)) {
         throw new HttpError(400, 'invalid address')
