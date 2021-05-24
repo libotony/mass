73a9e912581aea84342692d7f46d8a0e83b8d317
tony <liboliqi@gmail.com>
2021-05-24 15:33:30 +0800

    registry: add prices and adjust tokens structure

--- src/api/registry.ts
+++ src/api/registry.ts
@@ -6,14 +6,27 @@ import { Net } from '../net'
 const router = Router()
 export = router
 
-const net = new Net('https://vechain.github.io')
+const GhIO = new Net('https://vechain.github.io')
+const CoinGecko = new Net('https://api.coingecko.com')
+
 router.get('/tokens', try$(async (req, res) => {
     if (cache.has(keys.TOKENS)) {
         const tokens = cache.get(keys.TOKENS)
-        return res.json({tokens})
+        return res.json(tokens)
     } else {
-        const tokens = await net.http('GET', `token-registry/${process.env['NETWORK']=='testnet'?'test':'main'}.json`)
+        const tokens = await GhIO.http('GET', `token-registry/${process.env['NETWORK']=='testnet'?'test':'main'}.json`)
         cache.set(keys.TOKENS, tokens, 30*60*1000)
-        return res.json({tokens})
+        return res.json(tokens)
+    }
+}))
+
+router.get('/price', try$(async (req, res) => {
+    if (cache.has(keys.PRICE)) {
+        const price = cache.get(keys.PRICE)
+        return res.json(price)
+    } else {
+        const price = await CoinGecko.http('GET', 'api/v3/simple/price?ids=vechain,vethor-token&vs_currencies=btc')
+        cache.set(keys.PRICE, price, 60 * 1000)
+        return res.json(price)
     }
 }))
--- src/db-service/cache.ts
+++ src/db-service/cache.ts
@@ -7,6 +7,7 @@ export const keys = {
     BEST: 'BEST',
     LATEST: 'LATEST',
     TOKENS: 'TOKENS',
+    PRICE: 'PRICE',
     BLOCK_BY_ID: (blockID: string) => { return 'BLOCK_BY_ID' + blockID },
     BLOCK_BY_NUMBER: (blockNumber: number) => { return 'BLOCK_BY_NUMBER' + blockNumber },
     BLOCK_NEIGHBOUR: (blockNumber: number) => { return 'BLOCK_NEIGHBOUR' + blockNumber },
