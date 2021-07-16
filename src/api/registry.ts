e93d21238d7a546837f20ac8ab807853a1c0b1ae
tony <liboliqi@gmail.com>
2021-07-16 10:44:35 +0800

    registry: return last got data if http request failed

--- src/api/registry.ts
+++ src/api/registry.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
-import { try$ } from 'express-toolbox'
-import {cache, keys} from '../db-service/cache'
+import { HttpError, try$ } from 'express-toolbox'
+import { cache, keys } from '../db-service/cache'
 import { Net } from '../net'
 
 const router = Router()
@@ -10,23 +10,43 @@ const GhIO = new Net('https://vechain.github.io')
 const CoinGecko = new Net('https://api.coingecko.com')
 
 router.get('/tokens', try$(async (req, res) => {
+    let lastUpdated: null|object = null
     if (cache.has(keys.TOKENS)) {
         const tokens = cache.get(keys.TOKENS)
         return res.json(tokens)
     } else {
-        const tokens = await GhIO.http('GET', `token-registry/${process.env['NETWORK']=='testnet'?'test':'main'}.json`)
-        cache.set(keys.TOKENS, tokens, 30*60*1000)
-        return res.json(tokens)
+        try {
+            const tokens = await GhIO.http<object>('GET', `token-registry/${process.env['NETWORK'] == 'testnet' ? 'test' : 'main'}.json`)
+            cache.set(keys.TOKENS, tokens, 30 * 60 * 1000)
+            lastUpdated = tokens
+            res.json(tokens)
+        } catch (e) {
+            if (!!lastUpdated) {
+                res.json(lastUpdated)
+            } else {
+                throw new HttpError(500, 'failed to get registry')
+            }
+        }        
     }
 }))
 
 router.get('/price', try$(async (req, res) => {
+    let lastUpdated: null | object = null
     if (cache.has(keys.PRICE)) {
         const price = cache.get(keys.PRICE)
         return res.json(price)
     } else {
-        const price = await CoinGecko.http('GET', 'api/v3/simple/price?ids=vechain,vethor-token&vs_currencies=btc')
-        cache.set(keys.PRICE, price, 60 * 1000)
-        return res.json(price)
+        try {
+            const price = await CoinGecko.http<object>('GET', 'api/v3/simple/price?ids=vechain,vethor-token&vs_currencies=btc')
+            cache.set(keys.PRICE, price, 60 * 1000)
+            lastUpdated = price
+            res.json(price)
+        } catch (e) {
+            if (!!lastUpdated) {
+                res.json(lastUpdated)
+            } else {
+                throw new HttpError(500, 'failed to get registry')
+            }
+        }        
     }
 }))
